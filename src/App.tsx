f142e19dee01c1ba89bd7904a2a94cc91d4ac86e
Sergej Schmidt <sergej-schmidt@online.de>
2026-03-03 16:17:55 +0100

    layout fertiggestellt

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,93 @@
 // src/App.tsx
-import './App.css';
+import "./App.css";
+import { Search } from "lucide-react"; // Icon (npm i lucide-react)
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
     <div className="app-root">
+      {/* Header mit Suchleiste mittig */}
       <header className="app-header">
-        <h1>Meine App</h1>
+        <h1 className="flex-1">Meine App</h1>
+
+        {/* Suchleiste mittig */}
+        <div className="w-64 mx-auto">
+          <div className="relative">
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Suche Users, Posts..."
+              className="pl-10 w-full"
+            />
+          </div>
+        </div>
       </header>
 
       <div className="app-body">
         <aside className="app-sidebar">
           <nav>
-            <ul>
-              <li>Menüpunkt 1</li>
-              <li>Menüpunkt 2</li>
-              <li>Menüpunkt 3</li>
+            <ul className="space-y-1 p-2">
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/user-16-solid.png"
+                    alt="User"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>Users</span>
+                </Button>
+              </li>
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/post-add-rounded.png"
+                    alt="Posts"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>Posts</span>
+                </Button>
+              </li>
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/mode-comment-outline.png"
+                    alt="Comments"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>Comments</span>
+                </Button>
+              </li>
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/task-alt-rounded.png"
+                    alt="Albums"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>Albums</span>
+                </Button>
+              </li>
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/photo-solid.png"
+                    alt="Photos"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>Photos</span>
+                </Button>
+              </li>
+              <li>
+                <Button size="icon-lg" variant="ghost" className="w-full justify-start">
+                  <img
+                    src="/assets/task-alt-rounded.png"
+                    alt="ToDos"
+                    className="mr-2 h-4 w-4"
+                  />
+                  <span>ToDos</span>
+                </Button>
+              </li>
             </ul>
           </nav>
         </aside>
